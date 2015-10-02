2b9141feb12212caa518e071057848784578ca9c
Natalia Jastrzębska <nataliak@10g.pl>
2015-10-02 16:44:54 +0200

    It is possible to change owner and repo

--- Githuber/app/src/main/java/com/example/natalia/githuber/MainActivity.java
+++ Githuber/app/src/main/java/com/example/natalia/githuber/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.natalia.githuber;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,6 +11,9 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
@@ -29,6 +34,8 @@ public class MainActivity extends Activity {
 
     public static final String API_URL = "https://api.github.com";
     private static Contributor[] data;
+    String owner = "square";
+    String repo = "retrofit";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,13 +78,39 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        final Context context = this;
+
+        final Dialog dialog = new Dialog(context);
+        dialog.setContentView(R.layout.dialog_layout);
+        dialog.setTitle("Insert owner");
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
+        Button dialogButton = (Button) dialog.findViewById(R.id.dialogButtonOK);
+        // if button is clicked, close the custom dialog
+        dialogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                owner = ((EditText) dialog.findViewById(R.id.inputOwner)).getText().toString();
+                dialog.dismiss();
+                final Dialog secondDialog = new Dialog(context);
+                secondDialog.setContentView(R.layout.dialog_layout);
+                secondDialog.setTitle("Insert repo");
+                Button dialogButton = (Button) secondDialog.findViewById(R.id.dialogButtonOK);
+                dialogButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        repo = ((EditText) secondDialog.findViewById(R.id.inputOwner)).getText().toString();
+                        secondDialog.dismiss();
+                    }
+                });
+                secondDialog.show();
+            }
+        });
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            dialog.show();
             return true;
         }
 
@@ -104,7 +137,7 @@ public class MainActivity extends Activity {
 
         MainActivity.GitHub github = retrofit.create(MainActivity.GitHub.class);
 
-        Call<List<Contributor>> call = github.contributors("square", "retrofit");
+        Call<List<Contributor>> call = github.contributors(owner, repo);
         call.enqueue(new Callback<List<Contributor>>() {
             @Override
             public void onResponse(Response<List<Contributor>> response, Retrofit retrofit) {
